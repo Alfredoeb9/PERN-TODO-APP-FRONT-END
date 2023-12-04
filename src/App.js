2c6f4684d0489d7e73c9e5c9675f25e1be899743
Alfredoeb9 <alfredoeb96@gmail.com>
2023-12-03 23:13:12 -0800

    [query] quering get request

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
+  const [tasks, setTasks] = useState(null);
   const getData = async () => {
     const userEmail = 'alftest@test.com'
     try {
       const response = await fetch(`http://localhost:8000/todo/${userEmail}`)
       const json = await response.json()
-      console.log(json);
+      setTasks(json);
     } catch (err) {
       console.log('err', err)
     }
@@ -15,10 +16,17 @@ function App() {
 
   useEffect(() => {
     getData()
-  }, [])
+  }, [tasks])
+
+  // SORT BY DATE 
+  const sortedTasks = tasks?.sort((a,b) => new Date(a.date) - new Date(b.date))
+
   return (
     <div className="App">
       <h1>Barebones application built with React, Webpack, and babel </h1>
+      {sortedTasks?.map((task, i) => (
+        <div key={task.id}>{task.title}</div>
+      ))}
     </div>
   );
 }
