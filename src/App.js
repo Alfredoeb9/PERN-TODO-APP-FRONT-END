e76a6fcd82d630c4f51396af5141b7aae9610e45
Alfredoeb9 <alfredoeb96@gmail.com>
2023-12-03 20:14:20 -0800

    [api] setup up simple backend api to front end

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const getData = async () => {
+    const userEmail = 'alftest@test.com'
+    try {
+      const response = await fetch(`http://localhost:8000/todo/${userEmail}`)
+      const json = await response.json()
+      console.log(json);
+    } catch (err) {
+      console.log('err', err)
+    }
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
   return (
     <div className="App">
       <h1>Barebones application built with React, Webpack, and babel </h1>
